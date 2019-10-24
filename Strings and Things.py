a5ee14c8fd91c9c11f3092a8bf8302b40dae579d
100082395 <100082395@lubbockisd.net>
2019-10-24 15:27:03 -0500

    Wrote repetition along with function

---
+++ Strings and Things.py
@@ -0,0 +1,27 @@
+# Strings
+
+# Concatenation
+#   2 or more strings and put them together
+
+
+firstName = "Tulip"
+lastName = "Swing"
+
+print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(lastFirst)
+
+# Repetition
+
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + "your boat")
+    print("Gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
