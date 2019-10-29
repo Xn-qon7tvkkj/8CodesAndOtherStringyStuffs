83f508e9aa3f6607332d4da1fb0dfb2d9168fd3e
100082395 <100082395@lubbockisd.net>
2019-10-29 15:47:10 -0500

    added Decryption function

--- crypto.py
+++ crypto.py
@@ -13,3 +13,18 @@ def scramble2Encrypt(plainText):
         charCount = charCount + 1
     cipherText = oddChars + evenChars
     return cipherText
+
+def scramble2Decrypt(cipherText):
+    halfLength = len(cipherText) // 2
+    evenChars = cipherText[halfLength:]     # halflength to the end
+    oddChars = cipherText[:halfLength]      # 0 to halflength - 1
+    plainText = ""
+
+    for i in range(halfLength):             # split cipherText
+        plainText = plainText + evenChars[i]
+        plainText = plainText + oddChars[i]
+
+    if len(oddChars) < len(evenChars):      # if oddChars is less than evenChars
+        plainText = plainText + evenChars[-1]
+
+    return plainText
